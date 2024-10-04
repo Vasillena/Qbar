69d34beda891aec37f330849fca381e85644d717
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-10-04 14:14:55 +0300

    Update Video.tsx

--- app/components/Video.tsx
+++ app/components/Video.tsx
@@ -20,7 +20,6 @@ export function Video(): JSX.Element {
   }, [heroVideo]);
 
   return (
-    // <div className="relative mx-auto w-4/5 h-auto flex items-start justify-center overflow-hidden">
     <div className="relative w-full h-full flex items-start justify-center overflow-hidden">
       <video
         ref={videoRef}
@@ -43,8 +42,9 @@ export function Video(): JSX.Element {
         Your browser does not support the video tag.
       </video>
       <div className="absolute inset-0 pointer-events-none">
-        <div className="absolute inset-0 bg-gradient-to-r dark:from-[#222927] from-[#f2efea] to-transparent w-3/12"></div>
-        <div className="absolute inset-0 bg-gradient-to-l dark:from-[#222927] from-[#f2efea] to-transparent w-3/12 ml-auto"></div>
+        <div className="absolute inset-0 bg-gradient-to-r dark:from-[#222927] from-[#f2efea] to-transparent w-3/12" />
+        <div className="absolute inset-0 bg-gradient-to-l dark:from-[#222927] from-[#f2efea] to-transparent w-3/12 ml-auto" />
+        <div className="absolute inset-0 bg-gradient-to-b dark:from-[#222927] from-[#f2efea] to-transparent mb-auto h-20" />
       </div>
     </div>
   );
