a0186b58add8384dc1b8e24af89b6f75eb619b1a
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-10-16 21:04:23 +0300

    V

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -34,8 +34,8 @@ export function Hero(): JSX.Element {
   return (
     <>
       <div id="home" className="h-svh sm:h-screen flex flex-col justify-center">
-        <div className="max-w-[1440px] mx-auto mt-2 sm:mt-10 md:mt-20 pt-6 sm:pt-20 grid grid-cols-1 lg:grid-cols-2 px-8 lg:px-16 ">
-          <div className="text-center lg:text-left order-2 sm:order-1">
+        <div className="max-w-[1440px] mx-auto mt-2 sm:mt-10 lg:mt-20 pt-6 sm:pt-20 grid grid-cols-1 lg:grid-cols-2 px-8 lg:px-16 ">
+          <div className="text-center lg:text-left order-2 md:order-1">
             <h1
               className={`text-3xl font-bold sm:text-5xl lg:text-[46px] xl:text-6xl mt-12 sm:mt-0  ${oswald.className} hover-target`}
             >
@@ -58,7 +58,7 @@ export function Hero(): JSX.Element {
               {t("hero.button")}
             </Link>
           </div>
-          <div className="hidden min-[390px]:block sm:hidden lg:block h-[228px] lg:h-[590px] xl:h-[694px] relative order-1 sm:order-2 mx-auto sm:mx-0 -mb-10 min-[390px]:mb-0 pr-1">
+          <div className="hidden min-[390px]:block md:hidden lg:block w-[273px] h-[228px] sm:w-[385px] sm:h-[320px] lg:w-auto lg:h-[590px] xl:h-[694px] relative order-1 md:order-2 mx-auto md:mx-0 -mb-10 min-[390px]:mb-4 pr-1">
             <div className="lg:hidden">
               <Image
                 src={imageSrc}
